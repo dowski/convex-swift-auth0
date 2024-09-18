a00eb13d9ace3a5ab510b892accba6bb11ff5f1a
Christian Wyglendowski <christian@dowski.com>
2024-09-18 17:47:42 -0400

    Initial Swift Auth0 integration for Convex

--- Package.swift
+++ Package.swift
@@ -5,17 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "ConvexAuth0",
+    platforms: [.iOS(.v14),],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ConvexAuth0",
             targets: ["ConvexAuth0"]),
     ],
+    dependencies: [
+      .package(path: "../convex-swift"),
+      .package(url: "https://github.com/auth0/Auth0.swift", branch: "master"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ConvexAuth0"),
+            name: "ConvexAuth0",
+            dependencies: [.product(name: "ConvexMobile", package: "convex-swift"), .product(name: "Auth0", package: "Auth0.swift")]),
         .testTarget(
             name: "ConvexAuth0Tests",
             dependencies: ["ConvexAuth0"]
--- Sources/ConvexAuth0/ConvexAuth0.swift
+++ Sources/ConvexAuth0/ConvexAuth0.swift
@@ -1,2 +1,22 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import ConvexMobile
+import Auth0
+
+public class Auth0Provider : AuthProvider {
+  public init() {}
+  public func login() async throws -> Auth0.Credentials {
+    return try await Auth0.webAuth().start()
+  }
+  
+  public func extractIdToken(authResult: Auth0.Credentials) -> String {
+    return authResult.idToken
+  }
+  
+  public typealias T = Credentials
+  
+  public func logout() async throws {
+    try await Auth0.webAuth().clearSession()
+  }
+}
