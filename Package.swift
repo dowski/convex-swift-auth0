d221aa28a2b0b41a638e211bb69841b6341d4fd9
Christian Wyglendowski <christian@dowski.com>
2024-09-19 11:18:03 -0400

    Add support for using Auth0's CredentialsManager
    It stores data for prior logins in the keychain and allows a an automatic login if the user has valid saved credentials.

--- Package.swift
+++ Package.swift
@@ -4,27 +4,30 @@
 import PackageDescription
 
 let package = Package(
-    name: "ConvexAuth0",
-    platforms: [.iOS(.v14),],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "ConvexAuth0",
-            targets: ["ConvexAuth0"]),
-    ],
-    dependencies: [
-      .package(url: "https://github.com/dowski/convex-swift", exact: "0.1.0"),
-      .package(url: "https://github.com/auth0/Auth0.swift", exact: "2.9.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "ConvexAuth0",
-            dependencies: [.product(name: "ConvexMobile", package: "convex-swift"), .product(name: "Auth0", package: "Auth0.swift")]),
-        .testTarget(
-            name: "ConvexAuth0Tests",
-            dependencies: ["ConvexAuth0"]
-        ),
-    ]
+  name: "ConvexAuth0",
+  platforms: [.iOS(.v14)],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "ConvexAuth0",
+      targets: ["ConvexAuth0"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/dowski/convex-swift", exact: "0.2.0"),
+    .package(url: "https://github.com/auth0/Auth0.swift", exact: "2.9.0"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "ConvexAuth0",
+      dependencies: [
+        .product(name: "ConvexMobile", package: "convex-swift"),
+        .product(name: "Auth0", package: "Auth0.swift"),
+      ]),
+    .testTarget(
+      name: "ConvexAuth0Tests",
+      dependencies: ["ConvexAuth0"]
+    ),
+  ]
 )
--- Sources/ConvexAuth0/ConvexAuth0.swift
+++ Sources/ConvexAuth0/ConvexAuth0.swift
@@ -1,22 +1,43 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-import ConvexMobile
 import Auth0
+import ConvexMobile
 
-public class Auth0Provider : AuthProvider {
-  public init() {}
-  public func login() async throws -> Auth0.Credentials {
-    return try await Auth0.webAuth().start()
+private let SCOPES = "openid profile email offline_access"
+
+public class Auth0Provider: AuthProvider {
+  private let credentialsManager: CredentialsManager?
+
+  public init(enableCachedLogins: Bool = false) {
+    if enableCachedLogins {
+      credentialsManager = CredentialsManager(authentication: Auth0.authentication())
+    } else {
+      credentialsManager = nil
+    }
   }
-  
-  public func extractIdToken(authResult: Auth0.Credentials) -> String {
+
+  public func login() async throws -> Credentials {
+    let credentials = try await Auth0.webAuth().scope(SCOPES).start()
+    _ = credentialsManager?.store(credentials: credentials)
+    return credentials
+  }
+
+  public func loginFromCache() async throws -> Credentials {
+    guard credentialsManager != nil else {
+      fatalError("Can't call loginFromCache when not enabled")
+    }
+    return try await credentialsManager!.credentials()
+  }
+
+  public func extractIdToken(authResult: Credentials) -> String {
     return authResult.idToken
   }
-  
+
   public typealias T = Credentials
-  
+
   public func logout() async throws {
     try await Auth0.webAuth().clearSession()
+    await credentialsManager?.clear()
   }
 }
